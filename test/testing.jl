82ad438918aff8f8ee7212bf1487cc26d103979e
Bill <wherrera10@users.noreply.github.com>
2019-06-24 10:41:06 -1000

    Create testing.jl

---
+++ test/testing.jl
@@ -0,0 +1,24 @@
+using Test
+
+using BackedUpImmutable
+
+function testBackedUpImmutableDict()
+    fibr = BackedUpImmutableDict{String,Int64}(["a" => 0, "b" => 1, "c" => 1, "d" => 2,
+        "e" => 3, "f" => 5, "g" => 8, "h" => 13, "i" => 21, "j" => 34, "extra" => -1])
+
+    x = fibr["extra"]
+    fibr["extra"] = 0
+    y = fibr["extra"]
+    restore!(fibr, "extra")
+    z = fibr["extra"]
+    println("first $x, then $y, then back to $z")
+    
+    try
+        fibr["k"] = 55
+    catch
+        println("throws ok for readonly")
+    end
+
+end
+
+testBackedUpImmutableDict()
