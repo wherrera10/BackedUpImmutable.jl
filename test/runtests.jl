7373bb3d92d69e83b4b2cf14a2e0f5968a4b07c2
Bill <wherrera10@users.noreply.github.com>
2019-06-25 12:53:24 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,9 @@ using BackedUpImmutable
 d = StaticDict(["a" => 7, "d" => 2])
 @test d["d"] == 2
 
+d2 = StaticDict("a" => 7, "d" => 2, "e" => 4)
+@test d2["e"] != 3
+
 function testBackedUpImmutableDict()
     fibr = BackedUpImmutableDict{String,Int64}(["a" => 0, "b" => 1, "c" => 1, "d" => 2,
         "e" => 3, "f" => 5, "g" => 8, "h" => 13, "i" => 21, "j" => 34, "extra" => -1])
