fbf4107e5df67e717f92d22a8d842cba203c5257
Bill <wherrera10@users.noreply.github.com>
2019-06-24 20:24:56 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,19 @@ function testBackedUpImmutableDict()
     z = fibr["extra"]
     @test z == -1
     
-    @test_throws String  fibr["k"] = 55
+    @test_throws String begin fibr["k"] = 55 end
+    
+    # test alternative constructor
+   # fibr = BackedUpImmutableDict("a" => 0, "b" => 1, "c" => 1, "d" => 2,
+    #    "e" => 3, "f" => 5, "g" => 8, "h" => 13, "i" => 21, "j" => 34, "extra" => -1)
+    
+    fibr["a"] = 9
+    fibr["b"] = 7
+    
+    # test restore all to default
+    restoreall!(fibr)
+    
+    @test fibr["a"] == 0
 end
 
 testBackedUpImmutableDict()
