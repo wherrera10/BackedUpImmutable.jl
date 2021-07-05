15c5252d8f0331440f65809887f766e507f9c95d
Bill <wherrera10@users.noreply.github.com>
2021-07-05 12:48:40 -1000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,3 @@
 using Documenter, BackedUpImmutable
 
-makedocs(
-    modules = [BackedUpImmutable],
-    format = :html,
-    sitename = "BackedUpImmutable.jl",
-    pages = Any[
-        "Contents" => "contents.md",
-        "index.md",
-    ],
-)
-
-deploydocs(
-    repo = "github.com/wherrera10/BackedUpImmutable.jl.git",
-    target = "build",
-    deps   = nothing,
-    make   = nothing,
-)
+makedocs(sitename = "BackedUpImmutable Module Documentation", format = Documenter.HTML(prettyurls = false))
